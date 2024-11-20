62af81b14abe0b7cccf176822c56adfea2e6b5c3
Tomsta04 <23687106@stu.mmu.ac.uk>
2024-11-20 17:23:49 +0000

    Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI healthText;
+     [SerializeField] float moveSpeed = 6;
+    Animator anim;
+    Rigidbody2D rb;
+
+    int maxHealth = 100;
+    int currentHealth;
+
+    bool dead = false;
+
+    float moveHorizontal, moveVertical;
+    Vector2 movement;
+
+    int facingDirection = 1; // 1 = right, -1 = left
+
+    private void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (dead) 
+        {   
+            movement = Vector2.zero;
+            anim.SetFloat("Velocity", 0);
+            return;
+        }
+
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical"); 
+
+        movement = new Vector2(moveHorizontal, moveVertical).normalized;
+
+        anim.SetFloat("Velocity", movement.magnitude);
+
+        if (movement.x != 0)
+           facingDirection = movement.x > 0 ? 1 : -1;
+        
+        transform.localScale = new Vector2(facingDirection, 1);
+    }
+
+    private void FixedUpdate()
+     {
+        rb.velocity = movement * moveSpeed;
+     }
+
+     private void OnCollisionEnter2D(Collision2D collision)
+     {
+          // Check if we collide with an enemey
+     }
+
+     void Hit(int damage)
+     {
+        anim.SetTrigger("hit");
+        currentHealth -= damage;
+        healthText.text = Mathf.Clamp(currentHealth, 0, maxHealth).ToString();
+     }
+
+     void Die()
+     {
+        dead = true;
+        // Call GameOver
+     }
+
+}
